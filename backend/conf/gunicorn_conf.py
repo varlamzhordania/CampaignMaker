31fd8ebbaf369767cb589def6f57cb7738725f18
AmirBigdeli <141164942+varlamzhordania@users.noreply.github.com>
2023-10-22 21:00:16 +0400

    update files

---
+++ backend/conf/gunicorn_conf.py
@@ -0,0 +1,14 @@
+pythonpath = '/home/campaign/backend'
+bind = '0.0.0.0:8000'
+workers = 5
+timeout = 120
+#certfile = '/home/campaign/cert.crt'
+#keyfile = '/home/campaign/private.key'
+# Access log - records incoming HTTP requests
+#accesslog = "/var/log/gunicorn/gunicorn.access.log"
+# Error log - records Gunicorn server goings-on
+#errorlog = "/var/log/gunicorn/gunicorn.error.log"
+# Whether to send Django output to the error log
+#capture_output = True
+# How verbose the Gunicorn error logs should be
+# loglevel = "info"
